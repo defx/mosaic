04838b6593378107e468242d3116d720d9f98d4e
defx <mdonkin83@gmail.com>
2020-12-04 15:58:42 +0000

    tidy

--- customElementTagNames.js
+++
@@ -1,7 +0,0 @@
-const ANON = /<([a-z]\w*-\w*)/gm;
-
-function customElementTagNames(html = '') {
-  return (html.match(ANON) || []).map((v) => v.slice(1));
-}
-
-export default customElementTagNames;
