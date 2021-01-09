175ccdd1a32d9b9a96d9b28a71a1c90a4a1a707c
defx <mdonkin83@gmail.com>
2021-01-09 21:40:44 +0000

    rewrite to work as function pipeline

--- index.js
+++ index.js
@@ -1,151 +1,103 @@
 import chokidar from 'chokidar';
-import deepmerge from 'deepmerge';
 import express from 'express';
 import fs from 'fs';
 import glob from 'glob-promise';
 import path from 'path';
 
-const basename = (v) => path.basename(v, path.extname(v));
-const IDENTITY = (v) => v;
+const mosaic = (config) => {
+  let app, routes;
 
-let config = {};
-
-let cache = {
-  fragments: {},
-  templates: {},
-};
-
-const cacheFragment = (filename, fc) => {
-  fc.cache = fc.cache || {};
-  return fs.promises.readFile(filename, 'utf8').then((content) => {
-    fc.cache[basename(filename)] = { filename, content };
-  });
-};
-
-const cacheTemplate = (filename, fc) => {
-  fc.cache = fc.cache || {};
-  fs.promises.readFile(filename, 'utf8').then((content) => {
-    fc.cache[basename(filename)] = { filename, content };
-  });
-};
-
-const initialiseFragments = (fc) =>
-  glob(fc.input).then((files) =>
-    Promise.all(files.map((file) => cacheFragment(file, fc)))
-  );
-
-const initialiseTemplates = (fc) =>
-  glob(fc.templates).then((files) =>
-    Promise.all(files.map((file) => cacheTemplate(file, fc)))
-  );
-
-const watchFragments = (fc) => {
-  chokidar
-    .watch(fc.input)
-    .on('change', (filename) => cacheFragment(filename, fc).then(build));
-};
-
-const watchTemplates = (fc) => {
-  chokidar
-    .watch(config.templates)
-    .on('change', (filename) => cacheTemplate(filename).then(build));
-};
-
-const initialise = () =>
-  Promise.all([
-    initialiseTemplates(config),
-    ...config.fragments.map(initialiseFragments),
-  ]);
-
-const pageCache = {};
-
-const compile = () => {
-  let templates = config.cache;
-  let fragmentConfigs = config.fragments;
-
-  Object.values(templates).map((template) => {
-    let { filename, content } = template;
-    let tpl = fragmentConfigs.reduce((tpl, fc) => {
-      let { map = IDENTITY, filter = IDENTITY, reduce = IDENTITY, cache } = fc;
-      let fragments = filter(Object.values(cache), template).map(map);
-      return reduce(tpl, fragments);
-    }, content);
-
-    pageCache[basename(filename)] = {
-      ...template,
-      content: tpl,
-    };
-  });
-};
-
-const serve = () => {
-  const app = express();
-
-  if (config.staticDir) {
-    app.use(express.static(config.staticDir));
+  if (config.port) {
+    app = new express();
+    app.use(express.static('./'));
+    app.use(function (req, res, next) {
+      let k = req.originalUrl.split('?')[0];
+      if (k in routes) {
+        res.send(routes[k]);
+      } else {
+        next();
+      }
+    });
+    app.listen(config.port, () =>
+      console.log(`app listening on port ${config.port}`)
+    );
   }
 
-  app.get('/health', (req, res) => res.send('ok'));
+  const cache = {
+    /*
+        [mdContent]: [{ filepath, content }, { filepath, content }],
+        [template]: [{ filepath, content }]
+      */
+  };
 
-  app.use(function (req, res, next) {
-    let name = req.originalUrl.split('?')[0];
-    name = name === '/' ? 'index' : name.slice(1);
+  /*
+      transform cache into object expected by config...
 
-    let entry = pageCache[name];
+      - remove cache keys so that input name maps to array of objects
+      - if the input path wasn't a glob, then flatten
 
-    if (entry) {
-      res.send(entry.content);
+      */
+  const getCacheValues = () =>
+    Object.keys(cache).reduce((a, k) => {
+      let isGlob = config.input[k].includes('*');
+      let values = Object.values(cache[k]);
+      a[k] = isGlob ? values : values[0];
+      return a;
+    }, {});
+
+  const update = () => {
+    let values = getCacheValues();
+
+    let output = config.output.reduce(
+      (output, fn) => fn(output),
+      values
+    );
+    if (config.port) {
+      //@TODO: serve
+      routes = output.routes;
+      return Promise.resolve();
     } else {
-      next();
+      //save
+
+      //@TODO ensure directories exist
+
+      return Promise.all(
+        output.map(({ filepath, content }) =>
+          fs.promises.writeFile(filepath, content)
+        )
+      );
     }
-  });
+  };
 
-  return new Promise((resolve) => {
-    app.listen(config.port, () => {
-      console.log(`dev server listening on port ${config.port}`);
-      resolve();
-    });
+  const updateCache = (key, filepath) =>
+    fs.promises
+      .readFile(filepath, 'utf8')
+      .then((content) => {
+        cache[key][filepath] = { content, filepath };
+      });
 
-    // chokidar
-    //   .watch([config.templates, ...config.fragments.map(({ input }) => input)])
-    //   .on('change', compile)
-    //   .on('add', compile)
-    //   .on('unlink', compile);
-  });
+  const populateCache = () =>
+    Promise.all(
+      Object.entries(config.input).map(
+        ([type, globPath]) => {
+          cache[type] = {};
+          return glob(globPath).then((paths) =>
+            Promise.all(
+              paths.map((filepath) =>
+                updateCache(type, filepath)
+              )
+            )
+          );
+        }
+      )
+    );
+
+  return populateCache().then(update);
+  //   .then(() => {
+  //     if (config.watch) {
+  //       //@TODO: chokidar to watch -> update
+  //     }
+  //   });
 };
-
-const ensureDir = (filepath, dir = path.dirname(filepath)) =>
-  fs.promises.stat(dir).catch(() => fs.promises.mkdir(dir));
-
-const save = () => {
-  let dir = config.output.dir;
-
-  //@TODO: clean?
-
-  return ensureDir(dir).then(() =>
-    Object.values(pageCache).map(({ filename, content }) => {
-      let f = config.output.filename || filename;
-
-      if (typeof f === 'function') {
-        f = f(filename);
-      }
-
-      let filepath = path.join(dir, path.basename(f));
-
-      return fs.promises.writeFile(filepath, content);
-    })
-  );
-};
-
-function mosaic(c) {
-  config = c;
-  return initialise()
-    .then(compile)
-    .then(() => {
-      if (c.port) {
-        return serve(); //@TODO if(c.watch)
-      } else if (c.output) return save();
-    });
-}
 
 export default mosaic;
