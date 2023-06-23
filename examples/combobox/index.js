974a67fe1aea034a048a8540a85a7e33c00910eb
defx <mdonkin83@gmail.com>
2023-06-23 16:37:13 +0100

    test(combobox): define the component inside the test

--- examples/combobox/index.js
+++ examples/combobox/index.js
@@ -2,7 +2,7 @@ import { define } from "../mosaic.js"
 import { ComboBox } from "./combobox.js"
 
 const states =
-  "Alabama,Alaska,American Samoa,Arizona,Arkansas,California,Colorado,Conneticut,Delaware,District of Columbia"
+  `Alabama,Alaska,American Samoa,Arizona,Arkansas,California,ColoradoConnecticut,Delaware,District of Columbia,Florida,Georgia,Guam,Hawaii,Idaho,Illinois,Indiana,Iowa,Kansas,Kentucky,Louisiana,Maine,Maryland,Massachusetts,Michigan,Minnesota,Mississippi,Missouri,Montana,Nebraska,Nevada,New Hampshire,New Jersey,New Mexico,New York,North Carolina,North Dakota,Northern Marianas Islands,Ohio,Oklahoma,Oregon,Pennsylvania,Puerto Rico,Rhode Island,South Carolina,South Dakota,Tennessee,Texas,Utah,Vermont,Virginia,Virgin Islands,Washington,West Virginia,Wisconsin,Wyoming`
     .split(",")
     .map((value) => ({ id: `option_${value}`, value }))
 
--- examples/combobox/spec.js
+++ examples/combobox/spec.js
@@ -1,4 +1,20 @@
-import "./index.js"
+import { define } from "../mosaic.js"
+import { ComboBox } from "./combobox.js"
+
+const states =
+  `Alabama,Alaska,American Samoa,Arizona,Arkansas,California,ColoradoConnecticut,Delaware,District of Columbia,Florida,Georgia,Guam,Hawaii,Idaho,Illinois,Indiana,Iowa,Kansas,Kentucky,Louisiana,Maine,Maryland,Massachusetts,Michigan,Minnesota,Mississippi,Missouri,Montana,Nebraska,Nevada,New Hampshire,New Jersey,New Mexico,New York,North Carolina,North Dakota,Northern Marianas Islands,Ohio,Oklahoma,Oregon,Pennsylvania,Puerto Rico,Rhode Island,South Carolina,South Dakota,Tennessee,Texas,Utah,Vermont,Virginia,Virgin Islands,Washington,West Virginia,Wisconsin,Wyoming`
+    .split(",")
+    .map((value) => ({ id: `option_${value}`, value }))
+
+const optionTemplate = ({ id, value }) =>
+  `<li id="${id}" role="option">${value}</li>`
+
+define("combobox-example", () => {
+  return ComboBox({
+    optionTemplate,
+    options: states,
+  })
+})
 
 describe("combo-box", () => {
   let rootNode
